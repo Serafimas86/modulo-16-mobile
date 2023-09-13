a907e768d287530957199780a6e395bf7517d75d
Raffael Azevedo <raffael.azevedo@gmail.com>
2023-09-12 22:30:54 -0300

    remoção do primeiro método e adição do método goToLogin()

--- test/screens/home.screen.js
+++ test/screens/home.screen.js
@@ -1,11 +1,11 @@
 class homeScreen {
 
-    get enterFormsAddress() {
-        return $('~Forms')
+    async goToForms() {
+        await $('~Forms').click()
     }
 
-    async goToForms() {
-        await this.enterFormsAddress.click()
+    async goToLogin() {
+        await $('~Login').click()
     }
 }
 
