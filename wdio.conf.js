afb09474580dea8549b70bd9ba12c1f3f79d7d7e
Aline Serafim <aline.serafim86@gmail.comgit config --global user.name Aline>
2023-09-17 22:38:44 -0300

    alteracoes

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,26 +1,21 @@
-const {join} = require('path')
+const { join } = require('path')
 
 exports.config = {
     hostname: '127.0.0.1',
     port: 4723,
-    // o path do Appium a partir do 2.0 não é mais /wd/hub, mas sim só /
-    path: '/',
+    path: '/wd/hub',
     specs: [
         './test/specs/**/*.spec.js'
     ],
-    framework: 'mocha',
+
+    framework: 'jasmine',
     capabilities: [{
-        // algumas desired capabilites precisam do vendor prefix antes (i.e., appium:)
-        // https://appium.io/docs/en/2.1/guides/caps/
-        "platformName": "Android",
-        "appium:platformVersion": "12",
-        // aqui eu recomendo você dar uma olhada no UDID do seu aparelho
-        // https://www.bestinterviewquestion.com/question/what-is-udid-and-how-do-you-find-udid-in-appium-gmgte5343x9
-        // o nome do seu device com certeza não é esse - esse é o do professor, mas aparentemente ele não explicou isso direito
-        "appium:deviceName": "pixel2",
+        "appium:platformName": "Android",
+        "appium:platformVersion": "13.0",
+        "appium:deviceName": "samsung SM-A525M",
         "appium:automationName": "uiautomator2",
         "appium:app": join(process.cwd(), './app/android/Android-NativeDemoApp-0.4.0.apk'),
-        "appium:packageName": "com.wdiodemoapp",
-        "appium:appWaitActivity": 'MainActivity'
+        "appium:appPackage": "com.wdiodemoapp",
+        "appium:appWaitActivity": 'MainActivity',
     }]
 }
